5cda0721809e8fcdf89c494a5ce7a90518f31638
RequestFX <87524553+ViktorTrojan@users.noreply.github.com>
2023-04-12 12:25:48 +0200

    added basic test peer app

---
+++ App_PC/test_peer/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import PeerManager from './PeerManager'
+
+function App() {
+
+  return (
+    <div className="App">
+      <PeerManager />
+    </div>
+  )
+}
+
+export default App
